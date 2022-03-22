2fa935531cd2ea6f30fb6603479e121cac689c7b
werto87 <huhulasmichinruhe@web.de>
2022-03-22 11:04:50 +0100

    clean up

--- src/cxx/permutation.cxx
+++ src/cxx/permutation.cxx
@@ -62,3 +62,53 @@ numbersToCombine (long int k, size_t n)
   std::iota (indexes.begin (), indexes.end (), 0);
   return { combinationsNoRepetitionAndOrderDoesNotMatter (k / 2, indexes), combinationsNoRepetitionAndOrderDoesNotMatter (k / 2, std::vector<uint8_t> (indexes.begin (), indexes.begin () + static_cast<long int> (n) - (k / 2))) };
 }
+
+durak::Card
+idToCard (uint8_t id)
+{
+  return durak::Card{ boost::numeric_cast<u_int16_t> (id / 4), static_cast<durak::Type> (id % 4) };
+}
+
+uint8_t
+cardToId (durak::Card const &card)
+{
+  return boost::numeric_cast<uint8_t> (card.value * 4 + static_cast<uint8_t> (card.type));
+}
+
+std::vector<uint8_t>
+cardsToIds (std::vector<durak::Card> const cards)
+{
+  auto results = std::vector<uint8_t>{};
+  ranges::transform (cards, ranges::back_inserter (results), [] (durak::Card const &cards) { return cardToId (cards); });
+  return results;
+}
+
+std::vector<durak::Card>
+idsToCards (std::vector<uint8_t> const ids)
+{
+  auto results = std::vector<durak::Card>{};
+  ranges::transform (ids, ranges::back_inserter (results), [] (uint8_t id) { return idToCard (id); });
+  return results;
+}
+
+std::vector<durak::Card>
+compress (std::vector<durak::Card> cards)
+{
+  auto idsAndCards = std::vector<std::tuple<size_t, durak::Card> >{};
+  ranges::transform (cards, ranges::back_inserter (idsAndCards), [id = size_t{ 0 }] (durak::Card const &card) mutable { return std::tuple<size_t, durak::Card>{ id++, card }; });
+  ranges::sort (idsAndCards, {}, [] (std::tuple<size_t, durak::Card> const &idAndCard) { return std::get<1> (idAndCard); });
+  auto setToNumber = u_int16_t{ 0 };
+  auto numberToChange = std::get<1> (idsAndCards.front ()).value;
+  for (auto &[id, card] : idsAndCards)
+    {
+      if (card.value != numberToChange)
+        {
+          setToNumber++;
+          numberToChange = card.value;
+        }
+      card.value = setToNumber;
+    }
+  ranges::sort (idsAndCards);
+  ranges::transform (idsAndCards, cards.begin (), [] (std::tuple<size_t, durak::Card> const &idAndCard) { return std::get<1> (idAndCard); });
+  return cards;
+}
--- test/permutation.cxx
+++ test/permutation.cxx
@@ -1,6 +1,5 @@
 #include "src/cxx/permutation.hxx"
 #include "src/cxx/combination.hxx"
-#include "src/cxx/oldPermutation.hxx"
 // #include "src/cxx/util.hxx"
 #include <algorithm>
 #include <boost/numeric/conversion/cast.hpp>
@@ -54,68 +53,6 @@ printSymbol (uint8_t number)
   // clang-format on
 }
 
-durak::Card
-idToCard (uint8_t id)
-{
-  return durak::Card{ boost::numeric_cast<u_int16_t> (id / 4), static_cast<durak::Type> (id % 4) };
-}
-
-uint8_t
-cardToId (durak::Card const &card)
-{
-  return boost::numeric_cast<uint8_t> (card.value * 4 + static_cast<uint8_t> (card.type));
-}
-
-std::vector<uint8_t>
-cardsToIds (std::vector<durak::Card> const cards)
-{
-  auto results = std::vector<uint8_t>{};
-  ranges::transform (cards, ranges::back_inserter (results), [] (durak::Card const &cards) { return cardToId (cards); });
-  return results;
-}
-
-std::vector<durak::Card>
-idsToCards (std::vector<uint8_t> const ids)
-{
-  auto results = std::vector<durak::Card>{};
-  ranges::transform (ids, ranges::back_inserter (results), [] (uint8_t id) { return idToCard (id); });
-  return results;
-}
-
-std::vector<durak::Card>
-compress (std::vector<durak::Card> cards)
-{
-  auto idsAndCards = std::vector<std::tuple<size_t, durak::Card> >{};
-  ranges::transform (cards, ranges::back_inserter (idsAndCards), [id = size_t{ 0 }] (durak::Card const &card) mutable { return std::tuple<size_t, durak::Card>{ id++, card }; });
-  ranges::sort (idsAndCards, {}, [] (std::tuple<size_t, durak::Card> const &idAndCard) { return std::get<1> (idAndCard); });
-  auto setToNumber = u_int16_t{ 0 };
-  auto numberToChange = std::get<1> (idsAndCards.front ()).value;
-  for (auto &[id, card] : idsAndCards)
-    {
-      if (card.value != numberToChange)
-        {
-          setToNumber++;
-          numberToChange = card.value;
-        }
-      card.value = setToNumber;
-    }
-  ranges::sort (idsAndCards);
-  ranges::transform (idsAndCards, cards.begin (), [] (std::tuple<size_t, durak::Card> const &idAndCard) { return std::get<1> (idAndCard); });
-  return cards;
-}
-
-template <typename T, template <typename ELEM, typename ALLOC = std::allocator<ELEM> > class Container>
-std::ostream &
-operator<< (std::ostream &o, const Container<T> &container)
-{
-  typename Container<T>::const_iterator beg = container.begin ();
-  while (beg != container.end ())
-    {
-      o << "\n" << *beg++; // 2
-    }
-  return o;
-}
-
 TEST_CASE ("permut validation validation ", "[abc]")
 {
   size_t k = 4;
@@ -135,11 +72,6 @@ TEST_CASE ("permut validation validation ", "[abc]")
       std::cout << "results: " << compressedCombinations.size () << std::endl;
     }
 }
-
-// auto jack = u_int16_t{ 11 };
-// auto queen = u_int16_t{ 12 };
-// auto king = u_int16_t{ 13 };
-// auto ace = u_int16_t{ 14 };
 
 TEST_CASE ("compress", "[abc]")
 {
