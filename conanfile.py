6a5bbf2695e0c11024c7871bcb6e2b5c491debf1
walde <w-schneider1987@web.de>
2024-04-26 16:11:20 +0900

    added new way of using presets in cmake

--- conanfile.py
+++ conanfile.py
@@ -1,9 +1,14 @@
 from conan import ConanFile
-
+from conan.tools.cmake import CMakeToolchain
 
 class Project(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "CMakeToolchain", "CMakeDeps"
+    generators =  "CMakeDeps"
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.user_presets_path = False #workaround because this leads to useless options in cmake-tools configure
+        tc.generate()
 
     def configure(self):
         # We can control the options of our dependencies based on current options
@@ -14,9 +19,9 @@ class Project(ConanFile):
     def requirements(self):
         self.requires("boost/1.84.0",force=True)
         self.requires("catch2/2.13.9")
-        self.requires("durak/1.0.1")
+        self.requires("durak/1.0.5")
         self.requires("st_tree/1.2.1")
-        self.requires("small_memory_tree/4.0.1")
+        self.requires("small_memory_tree/4.0.2")
         self.requires("range-v3/0.12.0")
         self.requires("confu_soci/0.3.20")
         self.requires("magic_enum/[>=0.9.5 <10]")
