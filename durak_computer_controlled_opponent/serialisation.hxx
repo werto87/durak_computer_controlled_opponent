4993db24b2eb6a4decdcd163b50e7a380caa95e8
werto87 <huhulasmichinruhe@web.de>
2024-06-29 21:54:01 +0200

    added missing include

--- durak_computer_controlled_opponent/serialisation.hxx
+++ durak_computer_controlled_opponent/serialisation.hxx
@@ -29,6 +29,7 @@
   OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
+#include <bitset>
 #include <boost/numeric/conversion/cast.hpp>
 #include <cereal/cereal.hpp>
 #include <cereal/types/tuple.hpp>
