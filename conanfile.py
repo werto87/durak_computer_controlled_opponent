4a5d32043ecdb2b2b21889243b14c72598883c47
werto87 <huhulasmichinruhe@web.de>
2026-03-01 12:14:30 +0100

    dep update

--- conanfile.py
+++ conanfile.py
@@ -17,8 +17,8 @@ class Project(ConanFile):
         self.options["small_memory_tree"].with_st_tree = True
 
     def requirements(self):
-        self.requires("boost/1.86.0",force=True)
-        self.requires("durak/2.0.0")
+        self.requires("boost/1.90.0",force=True)
+        self.requires("durak/2.1.1")
         self.requires("st_tree/1.2.1")
         self.requires("small_memory_tree/7.0.5")
         self.requires("confu_soci/[<1]")
